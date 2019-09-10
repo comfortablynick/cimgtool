e4b173d4a700f0e79ab26101e16ecded9219094a
Nick Murphy <comfortablynick@gmail.com>
2019-09-10 08:39:32 -0500

    Add file ext to options

--- src/main.c
+++ src/main.c
@@ -10,9 +10,11 @@ typedef struct _options_t
     char *input_file;
     char *output_file;
     char *output_file_suffix;
+    char *file_extension;
     double pct_scale;
     int width;
     int height;
+    int quality;
     int verbosity;
     int no_op;
 } options_t;
@@ -30,6 +32,7 @@ static const char *help_text =
     "  -w, --width            Output width of image\n"
     "  -H, --height           Output height of image\n"
     "  -p, --pct-scale=<N>    Scale output to pct of original size\n"
+    "  -q, --quality=<N>      Encoding quality for JPEG images (default=85)\n"
     "  -s, --suffix=<TEXT>    Suffix to append to file name for edited file\n"
     "\n";
 
@@ -60,16 +63,18 @@ print_options_t(char *buf, size_t bufsize, options_t *options)
     snprintf(buf, bufsize,
              "Options:\n"
              "Input file:        %s\n"
+             "Input ext:         %s\n"
              "Output file:       %s\n"
              "Output suffix:     %s\n"
              "Pct scale:         %.1f\n"
+             "Quality:           %d\n"
              "Width:             %d\n"
              "Height:            %d\n"
              "Verbosity:         %d\n"
              "No-op:             %d",
-             options->input_file, options->output_file, options->output_file_suffix,
-             options->pct_scale, options->width, options->height, options->verbosity,
-             options->no_op);
+             options->input_file, options->file_extension, options->output_file,
+             options->output_file_suffix, options->pct_scale, options->quality, options->width,
+             options->height, options->verbosity, options->no_op);
 }
 
 int
@@ -81,13 +86,15 @@ parse_args(int argc, char **argv, options_t *options)
                                            {"help", no_argument, 0, 'h'},
                                            {"suffix", required_argument, 0, 's'},
                                            {"pct-scale", required_argument, 0, 'p'},
+                                           {"quality", required_argument, 0, 'q'},
                                            {"width", required_argument, 0, 'w'},
                                            {"height", required_argument, 0, 'H'},
                                            {0, 0, 0, 0}};
 
     int option_index = 0;
 
-    while ((choice = getopt_long(argc, argv, "Vhvnw:H:p:s:", long_options, &option_index)) != -1) {
+    while ((choice = getopt_long(argc, argv, "Vhvnw:H:p:s:q:", long_options, &option_index)) !=
+           -1) {
         switch (choice) {
         case 'V':
             fprintf(stderr, "%s %s\n", argv[0], PACKAGE_VERSION);
@@ -114,6 +121,9 @@ parse_args(int argc, char **argv, options_t *options)
             break;
         case 'H':
             options->height = atoi(optarg);
+            break;
+        case 'q':
+            options->quality = atoi(optarg);
             break;
         case ':':
             fprintf(stderr, "%s: option `-%c' requires an argument\n", argv[0], optopt);
@@ -174,61 +184,8 @@ humanize_bytes(size_t bytes_raw)
 }
 
 void
-get_new_filename(options_t *opts)
+init_log(options_t *opts)
 {
-    char *orig_file_name = NULL;
-    char orig_file_ext[8];
-
-    orig_file_name = g_path_get_basename(opts->input_file);
-    g_strlcpy(orig_file_ext, strrchr(orig_file_name, '.'), strlen(orig_file_ext));
-    char bare_name[strlen(orig_file_name) - strlen(orig_file_ext) + 1];
-    g_info("Output file not supplied; using suffix '%s'", opts->output_file_suffix);
-    g_strlcpy(bare_name, orig_file_name, sizeof(bare_name));
-    g_debug("Filename without ext: %s", bare_name);
-    g_info("File extension: %s", orig_file_ext);
-
-    // Assign new filename to opts struct
-    opts->output_file = (char *)malloc(strlen(opts->input_file) + strlen(opts->output_file_suffix));
-    sprintf(opts->output_file, "%s%s%s", bare_name, opts->output_file_suffix, orig_file_ext);
-    g_info("New filename: %s", opts->output_file);
-}
-
-int
-main(int argc, char **argv)
-{
-    VipsImage *image_in = NULL;
-    VipsImage *image_out = NULL;
-    int in_width = 0;
-    int in_height = 0;
-    int out_width = 0;
-    int out_height = 0;
-    char *in_size_human = NULL;
-    char *out_size_human = NULL;
-    char *size_delta_human = NULL;
-    char *in_buf;
-    size_t in_buf_size = 0;
-    size_t out_buf_size = 0;
-    size_t size_delta = 0;
-    void *out_buf;
-
-    // Init command line options
-    options_t *opts = malloc(sizeof(options_t));
-    opts->input_file = NULL;
-    opts->output_file = NULL;
-    opts->output_file_suffix = "_edited";
-    opts->pct_scale = 0;
-    opts->width = 0;
-    opts->height = 0;
-    opts->verbosity = 0;
-    opts->no_op = 0;
-
-    if (parse_args(argc, argv, opts)) {
-        fprintf(stderr, "%s: error parsing command-line arguments", argv[0]);
-        free(opts);
-        return 1;
-    }
-
-    // Logging
     // Set dummy handler for all levels
     // Set logging level based on verbosity flag
     g_log_set_handler(NULL, G_LOG_LEVEL_MASK, _glog_dummy_handler, NULL);
@@ -248,30 +205,81 @@ main(int argc, char **argv)
         setenv("G_MESSAGES_DEBUG", "all", 1);
         g_info("Verbosity level: %d", opts->verbosity);
     }
+}
+
+void
+get_new_filename(options_t *opts)
+{
+    char *orig_file_name = NULL;
+
+    orig_file_name = g_path_get_basename(opts->input_file);
+    char bare_name[strlen(orig_file_name) - strlen(opts->file_extension) + 1];
+    g_info("Output file not supplied; using suffix '%s'", opts->output_file_suffix);
+    g_strlcpy(bare_name, orig_file_name, sizeof(bare_name));
+
+    // Assign new filename to opts struct
+    opts->output_file = (char *)malloc(strlen(opts->input_file) + strlen(opts->output_file_suffix));
+    sprintf(opts->output_file, "%s%s%s", bare_name, opts->output_file_suffix, opts->file_extension);
+}
+
+int
+main(int argc, char **argv)
+{
+    VipsImage *image_in = NULL;
+    VipsImage *image_out = NULL;
+    int in_width = 0;
+    int in_height = 0;
+    int out_width = 0;
+    int out_height = 0;
+    char *in_size_human = NULL;
+    char *out_size_human = NULL;
+    char *size_delta_human = NULL;
+    char *in_buf;
+    size_t in_buf_size = 0;
+    size_t out_buf_size = 0;
+    size_t size_delta = 0;
+    void *out_buf;
+    char encode_opts[12];
+
+    // Init command line options
+    options_t *opts = malloc(sizeof(options_t));
+    opts->input_file = NULL;
+    opts->file_extension = NULL;
+    opts->output_file = NULL;
+    opts->output_file_suffix = "_edited";
+    opts->quality = 85;
+    opts->pct_scale = 0;
+    opts->width = 0;
+    opts->height = 0;
+    opts->verbosity = 0;
+    opts->no_op = 0;
+
+    if (parse_args(argc, argv, opts)) {
+        fprintf(stderr, "%s: error parsing command-line arguments", argv[0]);
+        free(opts);
+        return 1;
+    }
+
+    init_log(opts);
 
     // Deal with positional options
     opts->input_file = argv[optind++];
     opts->output_file = argv[optind++];
 
-    if (!opts->output_file) {
-        get_new_filename(opts);
+    if (!opts->input_file) {
+        fprintf(stderr, "%s: input file required\n", argv[0]);
+        exit(1);
     }
 
-    // Debug print for options
-    if (opts->verbosity > 1) {
-        char buf[250];
-        print_options_t(buf, sizeof(buf), opts);
-        g_debug("%s", buf);
-    }
+    opts->file_extension = strrchr(opts->input_file, '.');
+
+    if (!opts->output_file) get_new_filename(opts);
+
     if (optind < argc) {
         g_info("Additional non-option ARGV-elements: ");
         while (optind < argc) {
             g_info("%s ", argv[optind++]);
         }
-    }
-    if (!opts->input_file) {
-        fprintf(stderr, "%s: input file required\n", argv[0]);
-        exit(1);
     }
 
     if (VIPS_INIT(argv[0])) {
@@ -290,10 +298,20 @@ main(int argc, char **argv)
         vips_error_exit("error getting vips image from buffer");
     }
 
+    in_width = vips_image_get_width(image_in);
+    in_height = vips_image_get_height(image_in);
+
     if (opts->pct_scale) {
         g_info("Scaling image");
         opts->width = opts->pct_scale * in_width;
         opts->height = opts->pct_scale * in_height;
+    }
+
+    // Debug print for options
+    if (opts->verbosity > 1) {
+        char buf[300];
+        print_options_t(buf, sizeof(buf), opts);
+        g_debug("%s", buf);
     }
 
     // transform image
@@ -302,7 +320,9 @@ main(int argc, char **argv)
         vips_error_exit("error creating thumbnail");
     }
 
-    if (vips_image_write_to_buffer(image_out, ".jpg", &out_buf, &out_buf_size, NULL)) {
+    snprintf(encode_opts, sizeof(encode_opts), "%s[Q=%d]", opts->file_extension, opts->quality);
+    g_debug("Writing to buffer with suffix: %s", encode_opts);
+    if (vips_image_write_to_buffer(image_out, encode_opts, &out_buf, &out_buf_size, NULL)) {
         g_object_unref(image_out);
         vips_error_exit("error creating thumbnail");
     }
@@ -312,12 +332,8 @@ main(int argc, char **argv)
     out_size_human = humanize_bytes(out_buf_size);
     size_delta_human = humanize_bytes(size_delta);
 
-    in_width = vips_image_get_width(image_in);
-    in_height = vips_image_get_height(image_in);
     out_width = vips_image_get_width(image_out);
     out_height = vips_image_get_height(image_out);
-
-    g_debug("Input file size: %lu", in_buf_size);
 
     printf("%s"
            "Input file:        %s\n"
@@ -334,7 +350,6 @@ main(int argc, char **argv)
            opts->no_op ? "***Display results only***\n" : "", opts->input_file, in_width, in_height,
            in_size_human, opts->output_file, out_width, out_height, out_size_human,
            size_delta_human);
-
 
     if (!opts->no_op) {
         if (opts->output_file) {
